e3c4dd029d91ad4d2a3167b90d033b4e1888762d
elijahcroft <elijahcroft49@gmail.com>
2025-02-17 23:38:54 -0800

    Added some ui and started working on backend

--- background.js
+++ background.js
@@ -1,21 +1,45 @@
 
-async function getCurrentTab() {
-    const queryOptions = { active: true, lastFocusedWindow: true };
-    const [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-  }
-  
-  
-  chrome.cookies.getAll({}, (cookies) =>{
-    console.log(cookies);
-  }
+let cookies = [];
+let firstParty = [];
+let thirdParty = [];
+let currentTabUrl = "";
 
-  )
+
+chrome.tabs.onActivated.addListener((activeInfo) =>{
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        currentTabUrl = tabs[0].url;
+      });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
+    if(changeInfo.url){
+        currentTabUrl = changeInfo.url;
+    }
+});
+
+chrome.cookies.getAll({}, (cookieData) => {
+   
+    cookieData.forEach(cookie => {
+      if (cookie.url && cookie.url.includes(currentTabUrl)) {
+        firstParty.push(cookie);
+        
+      } else {
+        thirdParty.push(cookie);
+      }
+    });
+    
+    chrome.runtime.sendMessage({
+        "type": "fromBackground",
+        "firstPartyData": firstParty,
+        "thirdPartyData": thirdParty
+    });
+  });
   
 
   setInterval(() =>{
-    console.log(cookies);
+    console.log("third party cookies",thirdParty);
+    console.log("first party cookies",firstParty);
 
-  }, 2000);
+  }, 5000);
   
   
--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
     if(message.type === "fromBackground"){
-        console.log('Received third-party cookie domains:', message.data);
-        alert(message.data);
+        
+        console.log(message.data);
         
     }
     
